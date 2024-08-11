eee082efbc02aee8304d7d1bacaadd22896bed94
Manish Upadhyay <upadhyaymanish302@gmail.com>
2024-08-12 00:24:05 +0530

    Updated README file

--- todo-list-manager-backend/models/Task.js
+++ todo-list-manager-backend/models/Task.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const taskSchema = new mongoose.Schema({
   description: { type: String, required: true },
   dueDate: { type: Date },
-  status: { type: String, enum: ['pending', 'completed'], default: 'pending' },
+  status: { 
+    type: String, 
+    enum: ['pending', 'completed'],
+    default: 'pending' 
+  },
   tags: { type: [String] },
   deleted: { type: Boolean, default: false }
 });
